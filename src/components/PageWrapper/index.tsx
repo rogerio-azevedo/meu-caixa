6666b172991fad64c94ff4f50231b674a5bc9c2c
marcusmmmz <marcusizabel230@gmail.com>
2023-07-25 13:44:41 -0300

    fix: use correct page height in mobile

--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -1,20 +1,20 @@
-import React, { ReactNode } from "react"
-import { Inter } from "next/font/google"
+import React, { ReactNode } from "react";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 type WrapperProps = {
-  children: ReactNode
-}
+	children: ReactNode;
+};
 
 const PageWrapper = ({ children }: WrapperProps) => {
-  return (
-    <div
-      className={`flex flex-1 items-center justify-center px-8 ${inter.className}`}
-    >
-      <div className="flex flex-1 max-w-3xl h-screen">{children}</div>
-    </div>
-  )
-}
+	return (
+		<div
+			className={`flex flex-1 items-center justify-center px-8 ${inter.className}`}
+		>
+			<div className="flex flex-1 max-w-3xl h-[100dvh]">{children}</div>
+		</div>
+	);
+};
 
-export default PageWrapper
+export default PageWrapper;
